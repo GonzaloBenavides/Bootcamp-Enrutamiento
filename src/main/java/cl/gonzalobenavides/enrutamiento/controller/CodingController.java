5bd2785fcec78e37320706785069be5b07df0f78
GonzaloBenavides <gonzalo.benavides.bf@gmail.com>
2023-05-29 10:54:09 -0400

    Ajuste RestController

--- src/main/java/cl/gonzalobenavides/enrutamiento/controller/CodingController.java
+++ src/main/java/cl/gonzalobenavides/enrutamiento/controller/CodingController.java
@@ -4,24 +4,22 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class CodingController {
 	
 	@RequestMapping("/coding")
-	@ResponseBody
 	public String getCoding() {
 		return "¡Hola Coding Dojo!";
 	}
 	
 	@RequestMapping("/coding/python")
-	@ResponseBody
 	public String getCodingPython() {
 		return "¡Python/Django fue increíble!";
 	}
 	
 	@RequestMapping("/coding/java")
-	@ResponseBody
 	public String getCodingJava() {
 		return "¡Java/Spring es mejor!";
 	}
--- src/main/java/cl/gonzalobenavides/enrutamiento/controller/DojoController.java
+++ src/main/java/cl/gonzalobenavides/enrutamiento/controller/DojoController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DojoController {
 	
 	@RequestMapping("/{dojo}")
-	@ResponseBody
 	public String getDojo(@PathVariable String dojo) {
 		if(dojo.equals("dojo")) 
 			return "¡El Dojo es increíble!";
